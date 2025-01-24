56102749ed8372a370f341fc28e9f74641d866bd
Eric Melo <ericdev.ctrl@gmail.com>
2025-01-23 22:56:13 -0400

    feat: mock data

---
+++ lib/data/mock_data.dart
@@ -0,0 +1,64 @@
+Map<String, dynamic> mockData = {
+  "data": [
+    {
+      "id": "9b1de15a-d990-11ef-bdaa-9eb16cebfa3e",
+      "name": "Revisar conteúdos de estudo",
+      "type": "PERSONAL",
+      "isCompleted": true,
+    },
+    {
+      "id": "9b1de7ea-d990-11ef-bdaa-9eb16cebfa3e",
+      "name": "15 minutos de alongamento",
+      "type": "PERSONAL",
+      "isCompleted": false,
+    },
+    {
+      "id": "9b1deb8c-d990-11ef-bdaa-9eb16cebfa3e",
+      "name": "Planejar refeições",
+      "type": "PERSONAL",
+      "isCompleted": false,
+    },
+    {
+      "id": "9b1dee0c-d990-11ef-bdaa-9eb16cebfa3e",
+      "name": "Revisar os objetivos diários antes de dormir e adicionar novos",
+      "type": "PERSONAL",
+      "isCompleted": false,
+    },
+    {
+      "id": "9b1defce-d990-11ef-bdaa-9eb16cebfa3e",
+      "name": "Preparar apresentação",
+      "type": "JOB",
+      "isCompleted": false,
+    },
+    {
+      "id": "9b1df1a4-d990-11ef-bdaa-9eb16cebfa3e",
+      "name": "Criar ícones para o dashboard",
+      "type": "JOB",
+      "isCompleted": true,
+    },
+    {
+      "id": "9b1df5e6-d990-11ef-bdaa-9eb16cebfa3e",
+      "name": "Conferir histórias do dashboard",
+      "type": "JOB",
+      "isCompleted": false,
+    },
+    {
+      "id": "9b1dffd2-d990-11ef-bdaa-9eb16cebfa3e",
+      "name": "Regar plantas de casa",
+      "type": "HOME",
+      "isCompleted": false,
+    },
+    {
+      "id": "9b1df9ba-d990-11ef-bdaa-9eb16cebfa3e",
+      "name": "Dobrar roupas",
+      "type": "HOME",
+      "isCompleted": false,
+    },
+    {
+      "id": "9b1e039c-d990-11ef-bdaa-9eb16cebfa3e",
+      "name": "Preparar refeições da semana",
+      "type": "HOME",
+      "isCompleted": false,
+    }
+  ]
+};
